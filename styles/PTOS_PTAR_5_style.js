b287f1dedccddefdd9d34489f178b270e29e7108
aliciapalomino <86636680+aliciapalomino@users.noreply.github.com>
2021-07-02 04:35:16 -0500

    Add files via upload

---
+++ styles/PTOS_PTAR_5_style.js
@@ -0,0 +1,38 @@
+var size = 0;
+var placement = 'point';
+
+var style_PTOS_PTAR_5 = function(feature, resolution){
+    var context = {
+        feature: feature,
+        variables: {}
+    };
+    var value = ""
+    var labelText = "";
+    size = 0;
+    var labelFont = "16.900000000000002px \'Tahoma\', sans-serif";
+    var labelFill = "#000000";
+    var bufferColor = "#ffffff";
+    var bufferWidth = 1.0;
+    var textAlign = "left";
+    var offsetX = 8;
+    var offsetY = 3;
+    var placement = 'point';
+    if (feature.get("COD_PMONIT") !== null) {
+        labelText = String(feature.get("COD_PMONIT"));
+    }
+    var style = [ new ol.style.Style({
+        image: new ol.style.Circle({radius: 11.0 + size,
+            stroke: new ol.style.Stroke({color: 'rgba(20,0,0,1.0)', lineDash: null, lineCap: 'butt', lineJoin: 'miter', width: 1}), fill: new ol.style.Fill({color: 'rgba(115,255,223,1.0)'})}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement, bufferColor,
+                              bufferWidth)
+    }),new ol.style.Style({
+        image: new ol.style.Circle({radius: 3.3 + size,
+            stroke: new ol.style.Stroke({color: 'rgba(35,35,35,1.0)', lineDash: null, lineCap: 'butt', lineJoin: 'miter', width: 0}), fill: new ol.style.Fill({color: 'rgba(20,0,0,1.0)'})}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement, bufferColor,
+                              bufferWidth)
+    })];
+
+    return style;
+};
